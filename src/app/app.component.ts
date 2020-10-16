8fddf8408fb3be12265b99c01dc4dfe623f40226
unknown <SSA@DESKTOP-9DJK7CQ.PLAYRIGHT.local>
2020-10-16 22:29:07 +0200

    Raad het getal - basis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'raad-het-getal';
+  gameOn: boolean = false;
+//  randomNumber: number = Math.floor(Math.random()*100);
+  guessesLeft: number;
+//  lastGuess: number;
+//  message: string;
+
+  startGame(){
+    this.gameOn = true;
+    this.guessesLeft = 10;
+  }
+
+//  guess(newGuess){
+    //this.lastGuess = newGuess;
+  guess(){  
+    this.guessesLeft--;
+  }
+
+  endGame(){
+    this.gameOn = false;
+  //  this.guessesLeft = 0;
+  }
 }
