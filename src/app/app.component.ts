d493559811af992057a68819bef671c349270da8
unknown <SSA@DESKTOP-9DJK7CQ.PLAYRIGHT.local>
2020-10-17 13:50:42 +0200

    Raad het getal - eerste werkende versie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +8,38 @@ import { FormBuilder } from '@angular/forms';
 export class AppComponent {
   title = 'raad-het-getal';
   gameOn: boolean = false;
-//  randomNumber: number = Math.floor(Math.random()*100);
+  randomNumber: number;
   guessesLeft: number;
-//  lastGuess: number;
-//  message: string;
+  lastGuess: number;
+  message: string;
 
   startGame(){
     this.gameOn = true;
     this.guessesLeft = 10;
+    this.randomNumber = Math.floor(Math.random()*100);
+    this.message = `We hebben een getal gekozen, je hebt 10 kansen om het te raden. Succes!`;
   }
 
-//  guess(newGuess){
-    //this.lastGuess = newGuess;
-  guess(){  
+  guess(newGuess){
+    this.lastGuess = newGuess;
     this.guessesLeft--;
+    if(this.randomNumber == this.lastGuess){
+      this.message = `Proficiat, je hebt gewonnen,  ${this.lastGuess} is het juiste getal!`;
+      this.endGame();
+    }
+    else if(this.guessesLeft == 0){
+      this.message = `Je laatste gok was ${this.lastGuess}, je beurten zijn op, we zochten ${this.randomNumber}.`;
+      this.endGame();
+    }
+    else if(this.randomNumber > this.lastGuess) {
+      this.message = `Je laatste gok was ${this.lastGuess}, het getal dat we zoeken is groter.`;
+    }
+    else {
+      this.message = `Je laatste gok was ${this.lastGuess}, het getal dat we zoeken is kleiner.`;
+    }  
   }
 
   endGame(){
     this.gameOn = false;
-  //  this.guessesLeft = 0;
   }
 }
